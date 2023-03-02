fc12afd3e38ab2baa618c670fd6c8d4e06ea0055
Antonio Dell'Ava <antonio.dellava@gmail.com>
2023-03-02 11:54:16 +0100

    refines

--- e2e/src/e2e/app.cy.ts
+++ e2e/src/e2e/app.cy.ts
@@ -1,13 +1,9 @@
-import { getGreeting } from '../support/app.po';
+import { getHeader } from '../support/app.po';
 
 describe('profiles-app', () => {
   beforeEach(() => cy.visit('/'));
 
-  it('should display welcome message', () => {
-    // Custom command example, see `../support/commands.ts` file
-    cy.login('my-email@something.com', 'myPassword');
-
-    // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome profiles-app');
+  it('should display the Header', () => {
+    getHeader().contains('Profiles App');
   });
 });
